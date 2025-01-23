3d442280ddc61f0101008d6e7c852538bc212de0
Marc Diallo <mark.devguru@outlook.com>
2025-01-23 13:30:28 -0600

    prevents the default behavior ont the openModal function

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -20,10 +21,11 @@ const closeModal = function () {
 
 //Loops over each and every button show modal
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function (e) {
-    e.preventDefault();
-    openModal();
-  });
+  btnsOpenModal[i].addEventListener(
+    'click',
+
+    openModal
+  );
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
